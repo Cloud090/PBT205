0ad2199e12cbf6ab4ef3feb60deb29e22230042c
root <root@kali>
2024-10-02 08:21:25 -0400

    updated some chat_app code

--- chat_app_2.py
+++ chat_app_2.py
@@ -1,12 +1,4 @@
-'''
-devoriales.com
-https://devoriales.com/post/249
 
-This is the second part of the tutorial on how to create a chat application with Python and RabbitMQ. 
-In the first part, we created a simple chat application that allowed users to send messages to a queue. 
-In this part, we will create a chat application that allows users to send messages to each other.
-
-'''
 
 # chat_app.py
 import json
@@ -18,8 +10,8 @@ class ChatApp:
     # RabbitMQ connection parameters
     RABBITMQ_HOST = 'localhost'
     RABBITMQ_PORT = 5672
-    RABBITMQ_USERNAME = 'guest'
-    RABBITMQ_PASSWORD = 'guest'
+    RABBITMQ_USERNAME = 'admin'
+    RABBITMQ_PASSWORD = 'admin'
 
     def __init__(self, user_queue, partner_queue):
         self.user_queue = user_queue
@@ -34,39 +26,17 @@ class ChatApp:
 
 
     def clear_message(self, channel, method, properties, body):
-        '''
-        This function is called when a message is received by the consumer.
-        It prints the message to the console and acknowledges the message to the broker.
-        The broker will delete the message from the queue.
-        :param channel:
-        :param method:
-        :param properties:
-        :param body:
-        :return:
-        '''
+     
         print(f"Received message: {body.decode()}")
         channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
     def start_receiver(self, queue_name):
         print('start_receiver')
-        '''
-        This function starts a consumer that listens to the queue and calls the on_message function when a message is received.
-        It blocks the main thread, so it should be run in a separate thread.
-        :param queue_name:
-        :return:
-        '''
+      
         def callback(ch, method, properties, body):
             print('callback')
-            '''
-            This function is called when a message is received by the consumer.
-            The parameters are passed by the Pika library, not from the code.
-            :param ch:
-            :param method:
-            :param properties:
-            :param body:
-            :return:
-            '''
+            
             self.clear_message(ch, method, properties, body)
 
         # set credentials
